1c9f743f5f3bdf0604fdd2927653aabca992063b
Benoit Sagols <benoit.sagols@gmail.com>
2018-09-27 22:03:09 +0700

    Renamed View to Col + Introduced proper explicit package exports

--- index.js
+++
@@ -1,5 +0,0 @@
-import View from "./src/View";
-import Row from "./src/Row";
-
-export default View;
-export { Row };
--- src/Col.js
+++ src/Col.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { View as RNView } from "react-native";
+import { View } from "react-native";
 import { getColStyle, filterViewProps } from "./utils/spacing";
 import createStyleSheet from "./utils/stylesheet";
 
-export default function View(props) {
+export default function Col(props) {
   const style = getColStyle(props);
   const viewProps = filterViewProps(props);
 
   return (
-    <RNView {...viewProps} style={[createStyleSheet(style), viewProps.style]} />
+    <View {...viewProps} style={[createStyleSheet(style), viewProps.style]} />
   );
 }
