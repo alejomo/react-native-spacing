8c6ec45fe70c5b6a2c9f59c3a60f591f265fee1c
Benoit Sagols <benoit.sagols@gmail.com>
2019-01-22 15:34:29 +0700

    Whitelist standard margin padding properties

--- SpaceSheet.js
+++ SpaceSheet.js
@@ -4,17 +4,16 @@ import defaultStrategy from './strategy';
 export default class SpaceSheet extends Sheet {
   sizes = [];
   strategy = {};
-  isSpace = null;
+  isAlias = null;
 
   constructor(strategy = defaultStrategy) {
     super();
-
     this.strategy = strategy;
 
-    const spacings = Object.keys(strategy.spacings).join('|');
-    const sides = Object.keys(strategy.sides).join('|');
+    // Init
+    const { spacings, sides } = strategy;
 
-    this.isSpace = new RegExp(`^(${spacings})(${sides})?$`);
+    this.isAlias = createTest(Object.keys(spacings), Object.keys(sides));
   }
 
   setSpacing(spacing, range = 5) {
@@ -44,17 +43,23 @@ export default class SpaceSheet extends Sheet {
     // Generate style
     const style = {};
 
-    Object.keys(props)
-      .filter(name => this.isSpace.test(name))
-      .forEach(name => {
+    Object.keys(props).forEach(name => {
+      const size = props[name];
+      let prop;
+
+      if (this.isAlias.test(name)) {
         const spacing = this.strategy.getSpacing(name);
         const side = this.strategy.getSide(name);
-        const size = props[name];
 
-        style[`${spacing}${side}`] = this.sizes.length
-          ? this.sizes[size]
-          : size;
-      });
+        prop = `${spacing}${side}`;
+      } else if (isStandard.test(name)) {
+        prop = name;
+      }
+
+      if (prop) {
+        style[prop] = this.sizes.length ? this.sizes[size] : size;
+      }
+    });
 
     return style;
   }
@@ -67,7 +72,7 @@ export default class SpaceSheet extends Sheet {
     const props = {};
 
     Object.keys(view).forEach(name => {
-      if (this.isSpace.test(name)) {
+      if (this.isAlias.test(name) || isStandard.test(name)) {
         props[name] = view[name];
 
         delete view[name];
@@ -76,4 +81,11 @@ export default class SpaceSheet extends Sheet {
 
     return [view, props];
   }
+}
+
+const { spacings, sides } = defaultStrategy;
+const isStandard = createTest(Object.values(spacings), Object.values(sides));
+
+function createTest(spacings, sides) {
+  return new RegExp(`^(${spacings.join('|')})(${sides.join('|')})?$`);
 }
